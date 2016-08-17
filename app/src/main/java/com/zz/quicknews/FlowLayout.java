7a7be3af2df03716c7018744e98417c6ed5cb4db
yuchenfw <zz1181639528@163.com>
2016-08-17 19:27:55 +0800

    add LICENSE file

---
+++ app/src/main/java/com/zz/quicknews/FlowLayout.java
@@ -0,0 +1,102 @@
+package com.zz.quicknews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by pc on 2016/8/17.
+ */
+public class FlowLayout extends ViewGroup {
+    private float mVerticalSpacing; //每个item纵向间距
+    private float mHorizontalSpacing; //每个item横向间距
+    public FlowLayout(Context context) {
+        super(context);
+    }
+    public FlowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public void setHorizontalSpacing(float pixelSize) {
+        mHorizontalSpacing = pixelSize;
+    }
+    public void setVerticalSpacing(float pixelSize) {
+        mVerticalSpacing = pixelSize;
+    }
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int myWidth = r - l;
+
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+
+        int childLeft = paddingLeft;
+        int childTop = paddingTop;
+
+        int lineHeight = 0;
+
+        //根据子控件的宽高，计算子控件应该出现的位置。
+        for (int i = 0, childCount = getChildCount(); i < childCount; ++i) {
+            View childView = getChildAt(i);
+
+            if (childView.getVisibility() == View.GONE) {
+                continue;
+            }
+
+            int childWidth = childView.getMeasuredWidth();
+            int childHeight = childView.getMeasuredHeight();
+
+            lineHeight = Math.max(childHeight, lineHeight);
+
+            if (childLeft + childWidth + paddingRight > myWidth) {
+                childLeft = paddingLeft;
+                childTop += mVerticalSpacing + lineHeight;
+                lineHeight = childHeight;
+            }
+            childView.layout(childLeft, childTop, childLeft + childWidth, childTop + childHeight);
+            childLeft += childWidth + mHorizontalSpacing;
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int selfWidth = resolveSize(0, widthMeasureSpec);
+
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int childLeft = paddingLeft;
+        int childTop = paddingTop;
+        int lineHeight = 0;
+
+        //通过计算每一个子控件的高度，得到自己的高度
+        for (int i = 0, childCount = getChildCount(); i < childCount; ++i) {
+            View childView = getChildAt(i);
+            LayoutParams childLayoutParams = childView.getLayoutParams();
+            childView.measure(
+                    getChildMeasureSpec(widthMeasureSpec, paddingLeft + paddingRight,
+                            childLayoutParams.width),
+                    getChildMeasureSpec(heightMeasureSpec, paddingTop + paddingBottom,
+                            childLayoutParams.height));
+            int childWidth = childView.getMeasuredWidth();
+            int childHeight = childView.getMeasuredHeight();
+
+            lineHeight = Math.max(childHeight, lineHeight);
+
+            if (childLeft + childWidth + paddingRight > selfWidth) {
+                childLeft = paddingLeft;
+                childTop += mVerticalSpacing + lineHeight;
+                lineHeight = childHeight;
+            } else {
+                childLeft += childWidth + mHorizontalSpacing;
+            }
+        }
+
+        int wantedHeight = childTop + lineHeight + paddingBottom;
+        setMeasuredDimension(selfWidth, resolveSize(wantedHeight, heightMeasureSpec));
+    }
+}
